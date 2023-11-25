ecf803a851d4879ed901c90383ceca4f084c1f36
PoBe <you@example.com>
2023-11-25 09:16:43 +0100

    change WOW reporting interface

--- README.md
+++ README.md
@@ -11,12 +11,14 @@ This program just collects outdoor temp and wind metrics, but you may collect ot
 The observations are reported to the Met Office cloud service. For that you have to
 1) create an Met Office account(https://register.metoffice.gov.uk/WaveRegistrationClient/public/register.do?service=weatherobservations)
 2) create and define your weather station site. Instructions can be found here; https://about.metservice.com/our-company/your-weather/add-your-own-weather-station/
-3) observation are uploaded to the Met Office cloud service, apparently an Azure service. In order for that you will need to have a 'subscription key' that is obtained
-from the Met development portal https://mowowprod.portal.azure-api.net/ where you have to another account. See https://mowowprod.portal.azure-api.net/documentation/how-to-obtain-a-subscription-key
-for futher details.
+3) 3) It's a bit unclear what interface to use when reporting observations to the MET Office service. The one that seems to work is http://wow.metoffice.gov.uk/automaticreading.
+   The interface is described here; https://wow.metoffice.gov.uk/support/dataformats
 
-4) the REST API for uploading data to the Met Office Azure service are found here;
-https://mowowprod.portal.azure-api.net/documentation/table-of-contents
+   There is also another interface that allows you to report observation, however those observation reported does not seem to be connected with https://wow.metoffice.gov.uk/ information.
+   If you however like to explore that interface you can find more about it at  the Met development portal https://mowowprod.portal.azure-api.net/ where you have to another account. 
+   See https://mowowprod.portal.azure-api.net/documentation/how-to-obtain-a-subscription-key for futher details. 
+   The REST API for uploading data to the Met Office Azure service are found here;
+    https://mowowprod.portal.azure-api.net/documentation/table-of-contents
 
 The program will load a JSON file with your specific site, device and subscription data.
 the JSON syntax for this file **_auth_keys.json_** is
--- tfa-met-wow.py
+++ tfa-met-wow.py
@@ -1,9 +1,20 @@
 '''
 
+NOTE! NOTE! NOTE!
+This program reports observations to the MET Office WOW service, it however appears that
+this is not related to https://wow.metoffice.gov.uk/ weather MAP service. I however kept the
+program until I understand what the data is used for.
+
+If you are looking for reporting you observation to the MET Office WOW service and be related to your define
+site you should look at the program tfa-met.py.
+
+--------------------------------------------------------------------------------------
+
+
 This program report TFA WeatherHub (https://www.tfa-dostmann.de/en/produkte/weather-hub/) data to
 Met Office (meteorologic weather service) in UK https://www.metoffice.gov.uk/.
 
-The program collects weather observations data from the TFA cloud service https://www.data199.com/.
+The program collects weather observations data from the TFA cloud service https://www.data199.com.
 The format and API is found here; https://mobile-alerts.eu/info/public_server_api_documentation.pdf
 
 This program just collects outdoor temp and wind metrics, but you may collect other data depending on your configuration.
@@ -11,12 +22,11 @@ This program just collects outdoor temp and wind metrics, but you may collect ot
 The observations are reported to the Met Office cloud service. For that you have to
 1) create an Met Office account(https://register.metoffice.gov.uk/WaveRegistrationClient/public/register.do?service=weatherobservations)
 2) create and define your weather station site. Instructions can be found here; https://about.metservice.com/our-company/your-weather/add-your-own-weather-station/
-3) It's is a bit unclear what interface to use when reporting observations to the MET Office service. The one that seems to work is http://wow.metoffice.gov.uk/automaticreading.
-   The interface is described here; https://wow.metoffice.gov.uk/support/dataformats
+3) observation are uploaded to the Met Office cloud service, apparently an Azure service. In order for that you will need to have a 'subscription key' that is obtained
+from the Met development portal https://mowowprod.portal.azure-api.net/ where you have to another account. See https://mowowprod.portal.azure-api.net/documentation/how-to-obtain-a-subscription-key
+for futher details.
 
-   There is also another interface that allows you to report observation, however those observation reported does not seem to be connected with https://wow.metoffice.gov.uk/ information.
-   If you however like to explore that interface you can find more about it at  the Met development portal https://mowowprod.portal.azure-api.net/ where you have to another account.
-   See https://mowowprod.portal.azure-api.net/documentation/how-to-obtain-a-subscription-key for futher details. The REST API for uploading data to the Met Office Azure service are found here;
+4) the REST API for uploading data to the Met Office Azure service are found here;
 https://mowowprod.portal.azure-api.net/documentation/table-of-contents
 
 
@@ -26,7 +36,6 @@ import time
 import requests
 import datetime
 from datetime import timezone
-import urllib.parse
 import json
 
 windmeter_id = "0B6025223DD2"
@@ -38,13 +47,6 @@ longitude = 19.917539
 def timnow() -> str:
     _tim = datetime.datetime.now()
     return _tim.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
-
-
-def timDiff( time_stamp: int ) -> int:
-    _date_time = datetime.datetime.fromtimestamp( time_stamp )
-    _now = datetime.datetime.now();
-    _diff = _now - _date_time;
-    return _diff.seconds;
 
 
 def log( msg : str):
@@ -65,11 +67,13 @@ def getTfaData(auth_keys: dict)  -> dict:
     return data
 
 
-# YYYY-mm-DD HH:mm:ss
-def toUTCDateTime() ->str:
-    date_time = datetime.datetime.now(tz=timezone.utc)
-    timstr = date_time.strftime("%Y-%m-%d %H:%M:%S")
-    return urllib.parse.quote(timstr)
+def toUTCDateTime( timestamp : int = None ) ->str:
+    if not timestamp:
+        date_time = datetime.datetime.now(tz=timezone.utc)
+    else:
+        date_time = datetime.datetime.fromtimestamp( timestamp, tz=timezone.utc )
+    timstr = date_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return timstr
 
 def getDeviceData( tfa_data : dict, id : str ):
     arr = tfa_data["devices"]
@@ -81,28 +85,38 @@ def getDeviceData( tfa_data : dict, id : str ):
 def reportToWow( tfa_data : dict, auth_keys: dict):
     wind_dev = getDeviceData( tfa_data, windmeter_id)
     temp_dev = getDeviceData( tfa_data, outtemp_id)
-    if wind_dev:
-        wind_data = wind_dev["measurement"]
-        wind_data_age_sec = timDiff(wind_dev["lastseen"])
-    if temp_dev:
-        temp_data = temp_dev["measurement"]
-        temp_data_age_sec = timDiff(temp_dev["lastseen"])
-
     if wind_dev or temp_dev:
-        url = "http://wow.metoffice.gov.uk/automaticreading"
-        url += "?siteid=" + auth_keys['site_id']
-        url += "&siteAuthenticationKey=" + auth_keys['site_auth_key']
-        url += "&dateutc=" + toUTCDateTime()
+        headers = {'Content-Type': 'application/json','Ocp-Apim-Subscription-Key': auth_keys['subscription_key'],}
+        if wind_dev:
+            wind_data = wind_dev["measurement"]
+        if temp_dev:
+            temp_data = temp_dev["measurement"]
 
-        if wind_dev and (wind_data_age_sec < 7200):
-            url += "&winddir=" + str(int((22.5 * wind_data["wd"])))
-            url += "&windspeedmph=" + str(int(float(wind_data["ws"]) * 2.23694))
-            url += "&windgustmph=" + str(int((float(wind_data["wg"]) * 2.23694)))
-        if temp_data and (temp_data_age_sec < 7200):
-            url += "&tempf=" + str(int((float(temp_data["t1"]) * (9/5)) + 32))
-        url += "&softwaretype=TA-bridge-v1.0"
 
-        response = requests.get( url )
+        utc_date_time = toUTCDateTime()
+        wow_data = {}
+        wow_data["siteId"] = auth_keys['site_id']
+        wow_data["siteAuthenticationKey"] = auth_keys['site_auth_key']
+        wow_data["reportStartDateTime"] = utc_date_time
+        wow_data["reportEndDateTime"] = utc_date_time
+        wow_data["longitude"] = float(longitude)
+        wow_data["latitude"] = float(latitude)
+        if wind_data:
+            wow_data["windDirection"] = 22.5 * wind_data["wd"]
+            wow_data["windSpeed_MetrePerSecond"] = float(wind_data["ws"])
+            wow_data["windGust_MetrePerSecond"] = float(wind_data["wg"])
+        if temp_data:
+            wow_data["dryBulbTemperature_Celsius"] = float(temp_data["t1"])
+        wow_data["isLatestVersion"] = True
+        wow_data["isPublic"] = True
+        wow_data["observationType"] = 1
+        wow_data["softwareType"] = "TFA testing bridge V1.0"
+
+         # print( wow_data )
+
+        url = 'https://mowowprod.azure-api.net/api/Observations'
+
+        response = requests.post( url , json= wow_data, headers=headers )
         if (response.status_code >= 200) and (response.status_code <= 299):
             json_response_str = response.content.decode('utf8').replace("'", '"')
             log("wow response: " + json_response_str )
@@ -110,7 +124,6 @@ def reportToWow( tfa_data : dict, auth_keys: dict):
             log(" %%%% wow reporting failure status: " + str(response.status_code) + " reason: " + response.reason )
     else:
         log("failed to retreive data from TFA")
-
 
 def loadAuthKeys() -> dict:
     fd = open('auth_keys.json')
